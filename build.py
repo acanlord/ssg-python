97a306576335f339fd83e68436babbbf79bfab6c
Richard Houck <rdhouck@gmail.com>
2019-11-13 11:03:43 -0800

    udpated stuff

--- build.py
+++ build.py
@@ -1,35 +1,65 @@
-pages = [
-      {
-          "filename": "./content/index.html",
-          "output": "docs/index.html",
-          "title": "About Me",
-      },
-      {
-          "filename": "./content/projects.html",
-          "output": "docs/projects.html",
-          "title": "Projects",
-      },
-      {
+#! /usr/bin/python
 
-          "filename": "./content/blog.html",
-          "output": "docs/blog.html",
-          "title": "blog",
-      }
-      ]
+# Generate HTML content 
 
 
-def gen_html():
+pages = [ 
+    { 
+        "filename": "./content/index.html",
+        "output": "docs/index.html",
+        "title": "About Me",
+    },
+    {
+        "filename": "./content/projects.html",
+        "output": "docs/projects.html",
+        "title": "Projects",
+    },
+    {
+
+        "filename": "./content/blog.html",
+        "output": "docs/blog.html",
+        "title": "blog",
+    }
+    ]
+
+
+
+
+
+def get_template(template):
+    with open(template) as template_contents:
+        return template_contents.read()
+
+
+
+
+def gen_html(pages):
+    top = "./templates/top.html"
+    bottom = "./templates/bottom.html"
 
     for p in pages:
-        base = "./templates/base.html"
-        # Read in the entire template
-        template = open(base).read()
-        # Read in the content of the index HTML page
-        index_content = open(p["filename"]).read()
-        # Use the string replace
-        template = template.replace("{{title}}", p["title"])
-        finished_index_page = template.replace("{{content}}", index_content)
-        open(p["output"], "w+").write(finished_index_page)
+        with open(p["output"], 'w') as outfile:
+
+                # Get and dump 'top' template into 'output' file
+                top_html = get_template(top)
+                top_html = top_html.replace("{{title}}", p["title"])
+                outfile.write(top_html)
+
+                # Get and dump page.filename contents into page.output
+                with open(p["filename"]) as infile:
+                    outfile.write(infile.read())
+
+                # Get and dump 'bottom' template into 'output' file
+                outfile.write(get_template(bottom))
+    
+    return True
 
 
-gen_html()
+def main():
+    if gen_html(pages):
+        print('Done generating HTML files at docs/')
+    else:
+        print('oh shit')
+
+if __name__ == "__main__":
+    main()
