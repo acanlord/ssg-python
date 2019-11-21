07d180bcb078bcac829fadd9f2d620b87b988bce
Richard Houck <rdhouck@gmail.com>
2019-11-21 15:14:18 -0800

    updated stuff

--- build.py
+++ build.py
@@ -40,7 +40,7 @@ def gen_blog():
         partial = open(p["filename"]).read()
         open(p["output"], "w+").write(template)
 
-def fix_template():
+def fix_templates():
 
     for p in pages:
         partial = open(p["filename"]).read() 
@@ -49,12 +49,12 @@ def fix_template():
         template = template.render({'title': p['title']})
         open(p["output"], "w+").write(template)
 
-    for b in blog:
-        partial = open(p["filename"]).read() 
-        template_html = open("templates/base.html").read()
-        template = Template(template_html)
-        template = template.render({'title': b['title']})
-        open(b["output"], "w+").write(template)
+    # for b in blog:
+    #     partial = open(b["filename"]).read() 
+    #     blog_template = open("templates/blog.html").read()
+    #     template = Template(blog_template)
+    #     template = template.render({'title': b['title']})
+    #     open(b["output"], "w+").write(template)
 
 
 
@@ -62,7 +62,7 @@ def main():
     read_files()
     gen_html()
     gen_blog()
-    fix_template()
+    fix_templates()
     print("Your files have been generated")
 
 if __name__ == "__main__":
